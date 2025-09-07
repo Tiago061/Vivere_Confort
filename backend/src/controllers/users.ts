cdc74b87dc164959ff59f3b4e956d2f8876dab16
Tiago061 <thenriqueleite277@gmail.com>
2025-09-07 01:11:18 -0300

    feat(controllers): implemented methods createUser, getUserById, getUsers to class UsersController

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -2,4 +2,30 @@ import { prisma } from '../utils/prisma';
 
 export default class UsersController {
 
+     async getUsers() {
+        const users = await prisma.user.findMany()
+        return users
+    }
+
+    async getUserById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: { id  }
+        })
+
+        return user
+    }
+
+    async createUser(data: {firstname: string, lastname: string, age: number, email: string, cpf: string, }){
+
+        const user = await prisma.user.create({
+            data: {
+                firstName: data.firstname,
+                lastName: data.lastname,
+                email: data.email,
+                age: data.age,
+                cpf: data.cpf
+            }
+        })
+        return user
+    }
 }
