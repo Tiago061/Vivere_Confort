8b869663a3cd5b91d85cdaddfe0a19f2173155fa
Tiago061 <thenriqueleite277@gmail.com>
2025-09-07 01:09:29 -0300

    refactor(service): deleted path service

--- backend/src/services/users.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from '../utils/prisma';
-
-export default class UsersService{
-    async getUsers() {
-        const users = await prisma.user.findMany()
-        return users
-    }
-}
