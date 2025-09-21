df8b2351e457d291b2d2bb64f7ef1157be9ab29f
Tiago061 <thenriqueleite277@gmail.com>
2025-09-20 23:33:48 -0300

    refactor(controller): updated from braces to parentheses

--- backend/src/controllers/products.controller.ts
+++ backend/src/controllers/products.controller.ts
@@ -22,7 +22,7 @@ export class ProductsController {
         try {
             const { id }= req.params;
             const product = await this.productsService.getProductById(id)
-            res.json{product}
+            res.json(product)
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Internal Server error'})
