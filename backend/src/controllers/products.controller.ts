45b4e90d85abfa42028d7927c2c9403754c89767
Tiago061 <thenriqueleite277@gmail.com>
2025-09-20 23:31:10 -0300

    feat(controller): add methods update, delete, create, get products

---
+++ backend/src/controllers/products.controller.ts
@@ -0,0 +1,62 @@
+import { ProductsService } from "../services/products.service";
+import {Request, Response } from 'express'
+
+export class ProductsController {
+    private productsService: ProductsService;
+
+    constructor() {
+        this.productsService = new ProductsService();
+    }
+
+    async getProducts (req: Request, res: Response) {
+        try{
+            const products = await this.productsService.getProducts()
+            res.json(products)
+        }catch (error) {
+             console.error(error);
+            res.status(500).json({ error: 'Internal Server error'})
+        }
+    }
+
+    async getProductById(req: Request<{ id: string}>, res: Response) {
+        try {
+            const { id }= req.params;
+            const product = await this.productsService.getProductById(id)
+            res.json{product}
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server error'})
+        }
+    }
+
+    async createProduct(req: Request, res: Response) {
+        try{
+            const newProduct = await this.productsService.createProduct(req.body)
+            res.status(201).json(newProduct)
+        }catch(error){
+            console.error(error)
+            res.status(500).json({ error: 'Internal Server error'})
+        }
+    }
+    
+    async updateProduct(req: Request<{ id: string}>, res: Response) {
+        try{
+            const { id } = req.params
+            const updateProduct = await this.productsService.updateProduct(id , req.body)
+            res.json(updateProduct)
+        }catch(error){
+            console.error(error)
+            res.status(500).json({ error: 'Internal Server error'})
+        }
+    }
+
+    async deleteProduct(req: Request<{ id: string}>, res: Response){
+        try{
+            const { id } = req.params
+            const deleteProduct = await this.productsService.deleteProduct(id)
+            res.json(deleteProduct)
+        }catch(error){
+            console.error(error)
+        }
+    }
+}
