c1b6a5ff382d7ed101122e24e74d39bc634f4640
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-08 00:04:53 -0300

    feat(app): add register route app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import cors from 'cors'
 import usersRoutes from './routes/users.routes'
 import productsRoutes from './routes/products.routes'
 import addressRoutes from './routes/address.routes'
-
+import categoryRoutes from './routes/category.routes'
 config()
 
 const app: Application = express()
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/users', usersRoutes)
 app.use('/products', productsRoutes)
 app.use('/addresses', addressRoutes)
+app.use('/categories', categoryRoutes)
 
 app.get('/', async (_req: Request, res: Response) => {
         
