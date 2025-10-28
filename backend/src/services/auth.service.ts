aa78ff2d339ff685b19678c97b3feb753f108306
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-28 01:02:33 -0300

    fix(auth): include user role in JWT token and update signup method signature

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import { User } from "@prisma/client";
 
 export class AuthService {
-    async login(data:{ email: string, password: string }): Promise<{ user: Omit<User, 'password'>; token: string }> {
+    async login(data:{ email: string, password: string,  }): Promise<{ user: Omit<User, 'password'>; token: string }> {
         // Lógica de autenticação
         const user = await prisma.user.findUnique({ where: { email: data.email } });
         if(!user) throw new Error("User not found");
@@ -16,14 +16,14 @@ export class AuthService {
         const JWT_SECRET: string = process.env.JWT_SECRET || ''
         if (!JWT_SECRET) throw new Error('JWT_SECRET não definido')
 
-        const token = jwt.sign({ userId: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' })
+        const token = jwt.sign({ userId: user.id, email: user.email, role: user.role }, JWT_SECRET, { expiresIn: '1h' })
         
          const { password, ...userWithoutPassword } = user;
 
         return { user: userWithoutPassword, token };
     }
 
-    async signup(data: { firstName: string; lastName: string; age: number; email: string; cpf: string; password: string }): Promise<Omit<User, 'password'>>{
+    async signup(data: { firstName: string; lastName: string; age: number; email: string; cpf: string; password: string, role?: string; }): Promise<Omit<User, 'password'>>{
 
           const existingUser = await prisma.user.findFirst({
             where: {
@@ -39,7 +39,8 @@ export class AuthService {
         const newUser = await prisma.user.create({
             data:{
                 ...data,
-                password: hashedPassword
+                password: hashedPassword,
+                role: data.role || "USER",
             }
         })
 
