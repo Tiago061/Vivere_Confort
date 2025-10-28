c1831e822a7a588d0b5857314323451b1d51a56d
Tiago Henrique <thenriqueleite277@gmail.com>
2025-10-28 00:44:48 -0300

    fix(app): reorder imports and add auth routes to app configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
-import  express, { type Application, type Request, type Response } from 'express'
 import { config } from 'dotenv'
+import  express, { type Application, type Request, type Response } from 'express'
 import cors from 'cors'
 import usersRoutes from './routes/users.routes'
 import productsRoutes from './routes/products.routes'
 import addressRoutes from './routes/address.routes'
 import categoryRoutes from './routes/category.routes'
+import authRoutes from './routes/auth.routes'
 config()
 
 const app: Application = express()
@@ -14,10 +15,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))    
 
 
+app.use('/auth', authRoutes)
 app.use('/users', usersRoutes)
 app.use('/products', productsRoutes)
 app.use('/addresses', addressRoutes)
 app.use('/categories', categoryRoutes)
+
 
 app.get('/', async (_req: Request, res: Response) => {
         
